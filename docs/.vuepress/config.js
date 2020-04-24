96b4e73aaa060eba98dea269070289cdc1575d4f
Maneesh Sahu <maneesh@sahu.haus>
2020-04-24 14:53:40 -0700

    Revert "Ejected theme"
    This reverts commit 957c09e8cf293c20d352691460e9e5642d2efbeb.

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,66 +1,58 @@
 module.exports = {
   title: "Okta Multi Tenant Admin",
   description: "Identity Management for Multi-Tenant Applications",
-  //theme: "cool",
+
   themeConfig: {
-    repo: "udplabs/mta-docs",
-    logo:
-      "https://www.okta.com/sites/all/themes/Okta/images/logos/developer/Dev_Logo-02_Large.png",
-    lastUpdated: "Last Updated",
+    repo: 'udplabs/mta-docs',
+    logo: 'https://www.okta.com/sites/all/themes/Okta/images/logos/developer/Dev_Logo-02_Large.png',
+    lastUpdated: 'Last Updated',
     // defaults to false, set to true to enable
     editLinks: true,
     // custom text for edit link. Defaults to "Edit this page"
-    editLinkText: "Edit this page on GitHub",
-    docsDir: "docs",
+    editLinkText: 'Edit this page on GitHub',
+    docsDir: 'docs',
     smoothScroll: true,
-    nav: require("./nav/en"),
+    nav: require('./nav/en'),
     sidebar: {
-      "/guide/": getGuideSidebar(),
+      '/guide/': getGuideSidebar(),
     },
     algolia: {
-      apiKey: "191db45e6f87475d6a1256d5a4298324",
-      indexName: "udplabs_mta-docs",
-    },
+      apiKey: '191db45e6f87475d6a1256d5a4298324',
+      indexName: 'udplabs_mta-docs'
+    }
   },
   plugins: [
-    ["@vuepress/back-to-top", true],
-    [
-      "@vuepress/pwa",
-      {
-        serviceWorker: true,
-        updatePopup: true,
-      },
-    ],
-    ["@vuepress/medium-zoom", true],
-    [
-      "@vuepress/google-analytics",
-      {
-        ga: "UA-128189152-1",
-      },
-    ],
-    [
-      "container",
-      {
-        type: "vue",
-        before: '<pre class="vue-container"><code>',
-        after: "</code></pre>",
-      },
-    ],
-    [
-      "container",
-      {
-        type: "upgrade",
-        before: (info) => `<UpgradePath title="${info}">`,
-        after: "</UpgradePath>",
-      },
-    ],
-    ["flowchart"],
+    ['@vuepress/back-to-top', true],
+    ['@vuepress/pwa', {
+      serviceWorker: true,
+      updatePopup: true
+    }],
+    ['@vuepress/medium-zoom', true],
+    ['@vuepress/google-analytics', {
+      ga: 'UA-128189152-1'
+    }],
+    ['container', {
+      type: 'vue',
+      before: '<pre class="vue-container"><code>',
+      after: '</code></pre>'
+    }],
+    ['container', {
+      type: 'upgrade',
+      before: info => `<UpgradePath title="${info}">`,
+      after: '</UpgradePath>'
+    }],
+    ['flowchart']
   ],
-  extraWatchFiles: [".vuepress/nav/en.js", ".vuepress/nav/zh.js"],
+  extraWatchFiles: [
+    '.vuepress/nav/en.js',
+    '.vuepress/nav/zh.js'
+  ]
 };
 
 function getGuideSidebar() {
-  return ["setup"];
+  return [
+    'setup'
+  ]
 }
 
 /*function getGuideSidebar (groupA, groupB) {
--- docs/.vuepress/nav/en.js
+++ docs/.vuepress/nav/en.js
@@ -1,30 +1,14 @@
 module.exports = [
-  {
-    text: "Login",
-    link: "/login/",
-    meta: {
-      requiresAuth: false,
+    {
+      text: 'Guide',
+      link: '/guide/'
     },
-  },
-  {
-    text: "Guide",
-    link: "/guide/",
-    meta: {
-      requiresAuth: true,
+    {
+      text: 'API Reference',
+      link: '/api/'
     },
-  },
-  {
-    text: "API Reference",
-    link: "/api/",
-    meta: {
-      requiresAuth: true,
-    },
-  },
-  {
-    text: "Demo",
-    link: "https://okta-mta.idp.rocks",
-    meta: {
-      requiresAuth: false,
-    },
-  },
-];
+    { 
+      text: "Demo", 
+      link: "https://okta-mta.idp.rocks" 
+    }
+  ]
